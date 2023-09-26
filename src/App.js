618918ac3983c34dbfd745b0269c40ed924922c8
Rahf <rahfnoor@gmail.com>
2023-09-26 08:37:34 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import EventList from './EventList';
 import EventDetail from './EventDetail';
 import Checkout from './Checkout';
 import NotFound from './NotFound';
+import AppHeader from './components/AppHeader';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<EventList />} />
-        <Route path="/events/:eventId" element={<EventDetail />} />
-        <Route path="/events/:eventId/checkout" element={<Checkout />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <>
+    <AppHeader />
+    <div className="px-4">
+      <Router>
+        <Routes>
+          <Route path="/" element={<EventList />} />
+          <Route path="/events/:eventId" element={<EventDetail />} />
+          <Route path="/events/:eventId/checkout" element={<Checkout />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+    </>
   );
 }
 
---
+++ src/components/AppHeader.js
@@ -0,0 +1,15 @@
+const AppHeader = () => {
+  return (
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 bg-white mb-4 border-b border-gray-300">
+      <div className="flex h-16 justify-between">
+        <div className="flex">
+          <div className="flex flex-shrink-0 items-center">
+              First Help Challenge - Rahf Noor
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AppHeader;
